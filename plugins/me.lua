d49e629081037fab4a8be36214d2fabd5dda22d9
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-07-17 13:25:18 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,47 +1,74 @@
 do
-local TIME_CHECK = 60
-local function pre_process(msg)
-  local hash = 'per_sec'
-  local get = tonumber(redis:get(hash) or 0)
-  redis:setex(hash, TIME_CHECK, get)
-  return msg
+local function get_msgs_user_chat(user_id, chat_id)
+  local user_info = {}
+  local uhash = 'user:'..user_id
+  local user = redis:hgetall(uhash)
+  local um_hash = 'msgs:'..user_id..':'..chat_id
+  user_info.msgs = tonumber(redis:get(um_hash) or 0)
+  user_info.name = user_print_name(user)..' ['..user_id..']'
+  return user_info
+end
+local function chat_stat(chat_id, typee)
+  -- Users on chat
+local hash = ''
+if typee == 'channel' then
+hash = 'channel:'..chat_id..':users'
+else
+  hash = 'chat:'..chat_id..':users'
+end
+  local users = redis:smembers(hash)
+  local users_info = {}
+
+  -- Get user info
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
+  end
+
+  -- Sort users by msgs number
+  table.sort(users_info, function(a, b) 
+      if a.msgs and b.msgs then
+        return a.msgs > b.msgs
+      end
+    end)
+ 
+  local arian = '0'
+  local text = 'users in this chat \n'
+  for k,user in pairs(users_info) do
+    --text = text..user.name..' = '..user.msgs..'\n'
+      arian = arian + user.msgs
+  end
+  return arian
+end
+   local function rsusername_cb(extra, success, result)    
+     if success == 1 then
+      local user = result.peer_id
+      local name =  string.gsub(result.print_name, "_", " ")
+      local chatid = get_receiver(extra.msg)
+      local username = result.username
+      function round2(num, idp)
+      return tonumber(string.format("%." .. (idp or 0) .. "f", num))
+      end
+
+      local r = tonumber(chat_stat(extra.msg.to.id, extra.msg.to.type) or 0)
+
+      local hashs = 'msgs:'..result.peer_id..':'..extra.msg.to.id
+      local msgss = redis:get(hashs)
+      local percent = msgss / r * 100
+    return send_large_msg(chatid, " Your name : "..name.."\nThe number of messages sent by you : : "..msgss.." ("..round2(percent).."%)\n  تمام پیام های ارسال شده در گروه: "..r.."")
+
+    end
 end
 local function run(msg, matches)
-  if matches[1] == "me" then
-    if msg.to.type == "user" then
-      return msg
-    end
-    local hash = 'msgs:'..msg.from.id..':'..msg.to.id
-	local msgsend = redis:get(hash)
-	local r = tonumber(redis:get('cis_msg:'..msg.to.id))
-	local t = r * 100
-	local percent = math.floor((msgsend) / r * 100)
-	return reply_msg(msg.id, msg.from.print_name.." You've sent "..msgsend.."("..percent.."%) and this group has "..r.." messages", ok_cb, true)
-  end
-  if matches[1] == "botload" and is_admin(msg) then
-    local hash = 'total:msg'
-    local get = tonumber(redis:get(hash))
-    local per_sec = tonumber(redis:get('per_sec'))
-    return per_sec.." msgs/s\n"..get.." Message since last restart"
-  end
-  if matches[1] == "restart" and is_sudo(msg) then
-    local hash = 'total:msg'
-    redis:del(hash)
-    return "See ya later =)"
-  end 
+local chat_id = msg.to.id
+--return chat_stat(chat_id)
+  resolve_username(msg.from.username, rsusername_cb, {msg=msg})
 end
-
-return {
-  patterns = {
-    "^[/!#](me)$",
-    "^[/!#](botload)$",
-    "^[/!#](restart)$",
-    "^(me)$",
-    "^(botload)$",
-    "^(restart)$",
-  },
-  run = run,
-  pre_process = pre_process
-}
-
+  return {
+    patterns = {
+      "^[!/](me)$",
+    },
+    run = run
+  }
 end
