f084a5e08b196d1a97e815b6fe5392a9a8a11c27
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-08-17 12:44:25 +0430

    Update love.lua

--- plugins/love.lua
+++ plugins/love.lua
@@ -1,27 +1,25 @@
 function run(msg, matches)
-if matches[1] == 'lovep' then
-  local text1 = matches[2]
-  local text2 = matches[3]
-  local text3 = matches[4]
-  local url = "http://www.iloveheartstudio.com/-/p.php?t="..text1.."%0D%0A%EE%BB%AE%0D%0A"..text2.."&bc=F7FF82&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w="..text3.."&ps=sq"
-  local file = download_to_file(url,'love.png')
-   send_photo(get_receiver(msg),file,ok_cb,true)
+		local text1 = matches[1]
+        local text2 = matches[2]
+		local url = "http://www.iloveheartstudio.com/-/p.php?t="..text1.."%20%EE%BB%AE%20"..text2.."&bc=FFFFFF&tc=000000&hc=ff0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+		 local  file = download_to_file(url,'love.webp')
+			send_document(get_receiver(msg), file, ok_cb, false)
+
+        
 end
-if matches[1] == 'love' then
-  local text1 = matches[2]
-  local text2 = matches[3]
-  local text3 = matches[4]
-  local url = "http://www.iloveheartstudio.com/-/p.php?t="..text1.."%0D%0A%EE%BB%AE%0D%0A"..text2.."&bc=F7FF82&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w="..text3.."&ps=sq"
-  local files = download_to_file(url,'sticker.webp')
-   send_document(get_receiver(msg),files,ok_cb,true)
-   end
-   end
+
 
 return {
+  description = "تبدیل متن به لوگو",
+  usage = {
+    "/love text text: ساخت لوگو",
+  },
   patterns = {
-   "^([Ll][Oo][vV][Ee][Pp]) (.*) (.*) (.*)$",
-   "^([Ll][Oo][Vv][Ee]) (.*) (.*) (.*)$",
+   "^love (.+) (.+)$",
   },
   run = run
 }
---Edit by @p0kerfa3e
+-- by @sudo1
+-- ch: @specialteam_ch
+-- کپی بدون ذکر منبع حرام است.
+    
