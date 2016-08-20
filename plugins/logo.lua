23d5530a78046cb56076918379045be190f9c7e4
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-08-20 13:59:29 +0430

    Create logo.lua

---
+++ plugins/logo.lua
@@ -0,0 +1,23 @@
+function run(msg, matches)
+  local sudo1 = matches[1]
+local sudo2 = matches[2]
+  local url = "http://api.iteam-co.ir/imgmaker.php?text="..sudo1.."&size="..sudo2..""
+   local file = download_to_file(url,'iteam.jpg')
+   send_photo2(get_receiver(msg), file, "لطفا در کانال زیر عضو شوید:\n@SUDO_TM", rmtmp_cb, cb_extra)
+
+        
+end
+
+
+return {
+  description = "تبدیل متن به لوگو",
+  usage = {
+    "ساخت لوگو",
+  },
+  patterns = {
+   "^logo (.*) (.*)$"
+  },
+  run = run
+}
+--@silveloper_mental
+--devilteam(-SBSS)
