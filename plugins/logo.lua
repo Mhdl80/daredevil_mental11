f4671194e46b41b94e7bb1feaa9d1050345d4b4c
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-08-20 19:17:32 +0430

    Update logo.lua

--- plugins/logo.lua
+++ plugins/logo.lua
@@ -3,7 +3,7 @@ function run(msg, matches)
 local sudo2 = matches[2]
   local url = "http://api.iteam-co.ir/imgmaker.php?text="..sudo1.."&size="..sudo2..""
    local file = download_to_file(url,'iteam.jpg')
-   send_photo2(get_receiver(msg), file, "لطفا در کانال زیر عضو شوید:\n@SUDO_TM", rmtmp_cb, cb_extra)
+   send_photo2(get_receiver(msg), file, rmtmp_cb, cb_extra)
 
         
 end
