4b0d183459e1d7e35ee44017c83c0164fdd5fa49
Mhdl80 <Mahdi.shabanii80@gmail.com>
2016-08-21 20:52:47 +0430

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -3,7 +3,7 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "ارتباط وجود ندارد" end
 local jdat = json:decode(url)
-local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@avast_Team'
+local text = '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@silveloper_mental'
 return text
 end
 return {
